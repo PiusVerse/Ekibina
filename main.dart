f4297e0a017053697910c41085cbd22926670746
PIUS Kawenja <88225131+PiusVerse@users.noreply.github.com>
2022-02-14 15:57:52 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,34 +1,161 @@
-import 'package:eSacco/_user_view.dart';
-import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:firebase_core/firebase_core.dart';
-import 'welcome_screen.dart';
-import 'home_screen.dart';
-import 'signup_screen.dart';
-import 'login_screen.dart';
-import 'choose_kibiina.dart';
-import 'register_kibiina.dart';
+// import 'package:hive/hive.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(eSacco());
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:profile_page_application/models/Account.dart';
+
+import './editProfile.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  // Hive.registerAdapter<Account>(AccountAdapter());
+  await Hive.openBox("profile");
+  runApp(Profile());
 }
 
-class eSacco extends StatelessWidget {
-  // This widget is the root of your application.
+class Profile extends StatefulWidget {
+  @override
+  MyProfile createState() => MyProfile();
+}
+
+class MyProfile extends State<Profile> {
+  String username = 'pius';
+  // MyProfile({required this.username});
+  var myBirthday = 'DATE-of-Birth';
+  buildCountColumn(String label, count) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          count.toString(),
+          style: const TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4.0),
+          child: Text(
+            label,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 15.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  //editProfile() {
+  // var userId = 'pius';
+  //Navigator.push(context,
+  //  MaterialPageRoute(builder: (context) => EditProfile(user: userId)));
+  //}
+
+  Container buildButton(String word, BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 8.0),
+      // ignore: deprecated_member_use
+      child: FlatButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => EditProfile()),
+          );
+        },
+        child: Container(
+          width: 250.0,
+          height: 27.0,
+          child: Text(
+            word,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(color: Colors.blue),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildProfileButton(BuildContext context) {
+    return buildButton('Edit Profile', context);
+  }
+
+  buildProfileHeader() {
+    Box store = Hive.box('profile');
+    return FutureBuilder(
+      // future: usersRef,
+      builder: (context, snapshot) {
+        String userID = username;
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 40.0,
+                    backgroundColor: Colors.grey,
+                    // backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              //buildCountColumn('Days in', 0),
+                              buildCountColumn('Days Remaining', 0),
+                              buildCountColumn('Debt Status', 0),
+                            ],
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+              store.isNotEmpty
+                  ? Card(
+                      child: ListTile(
+                        title: Text(store.get('name')),
+                        subtitle: Text(store.get('dob')),
+                        trailing: Text(store.get('work')),
+                      ),
+                    )
+                  : Text("No data"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildProfileButton(context),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: 'welcome_screen',
-      routes: {
-        'welcome_screen': (context) => WelcomeScreen(),
-        'sign_up_screen': (context) => SignUpScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'home_screen': (context) => HomeScreen(),
-        'choose_kibiina': (context) => ChooseKibiina(),
-        'register_kibiina': (context) => RegisterKibiina(),
-      },
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        children: <Widget>[buildProfileHeader()],
+      ),
+    ));
   }
 }
